2646f57f1dee3632fe02f0f8d94d2d826fd1ce53
hesam dehghanmoshtaghin <hdm.hesam@gmail.com>
2023-08-05 17:44:56 +0200

    design

--- src/components/CountriesList.js
+++ src/components/CountriesList.js
@@ -1,25 +1,32 @@
 import { Link } from "react-router-dom";
 
-function CountriesList(props){
+function CountriesList(props) {
     const { countryList } = props;
-    return(
-        <div className='col-5' style={{maxHeight: "90vh", overflow: "scroll"}}>
-
-                <li>
-                    <div className="list-group">
-                        {countryList.map(country => {
-                            return (
-                            <div>
-                                <img src={`https://flagpedia.net/data/flags/icon/72x54/${country.alpha2Code.toLowerCase()}.png`} alt='country-flag'/>
-                                <ul key={country.alpha3Code} className='list-group-item list-group-item-action'><Link to={`/countries/${country.alpha3Code}`}>{country.name.common}</Link></ul>
-                            </div>)
-                        })}
+        return (
+            <div
+            className="col-5"
+            style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center'
+            }}
+            >
+            <div className="list-group">
+                {countryList.map((country) => {
+                return (
+                    <div key={country.alpha3Code}>
+                    <img
+                        src={`https://flagpedia.net/data/flags/icon/72x54/${country.alpha2Code.toLowerCase()}.png`}
+                        alt="country-flag"
+                    />
+                    <ul className="list-group-item list-group-item-action">
+                        <Link to={`/countries/${country.alpha3Code}`}>
+                        {country.name.common}
+                        </Link>
+                    </ul>
                     </div>
-                </li>
-
+                );
+                })}
+            </div>
         </div>
-    )
-
-}
+    );
+    }
 
 export default CountriesList;
--- src/components/CountryDetails.js
+++ src/components/CountryDetails.js
@@ -16,39 +16,40 @@ function CountryDetails(props){
 
     return (
 
-        <div className="col-7">
+        <div className="col-7" >
             <img src={`https://flagpedia.net/data/flags/icon/72x54/${foundCountry.alpha2Code.toLowerCase()}.png`} width={150} alt='country'/>
-            <h1>{foundCountry.name.common}</h1>
-            <table className="table">
-                <thead></thead>
-                <tbody>
-                    <tr>
-                        <td style={{width: '30%'}}>Capital</td>
-                        <td>{foundCountry.capital}</td>
-                    </tr>
-                    <tr>
-                    <td>Area</td>
-                    <td>
-                        {foundCountry.area}
-                        <sup>2</sup>
-                    </td>
-                    </tr>
-                    <tr>
-                    <td>{foundCountry.borders.length !== 0 ? "Borders":"No Adjacent Countries"}</td>
-                    <td>
-                        <ul>
+    <h1>{foundCountry.name.common}</h1>
+    <table className="table">
+        <thead></thead>
+        <tbody>
+            <tr>
+                <td style={{ width: '30%' }}>Capital</td>
+                <td>{foundCountry.capital}</td>
+            </tr>
+            <tr>
+                <td>Area</td>
+                <td>
+                    {foundCountry.area}
+                    <sup>2</sup>
+                </td>
+            </tr>
+            <tr>
+                <td>{foundCountry.borders.length !== 0 ? "Borders" : "No Adjacent Countries"}</td>
+                <td>
+                    <ul>
                         {foundCountry.borders.map(border => {
-                            const countryInBorder = countryList.filter(country =>{
+                            const countryInBorder = countryList.filter(country => {
                                 return country.alpha3Code === border
                             })
                             return <li key={countryInBorder[0].alpha3Code}><Link to={`/countries/${countryInBorder[0].alpha3Code}`}>{countryInBorder[0].name.common}</Link></li>
                         })}
-                        </ul>
-                    </td>
-                    </tr>
-                </tbody>
-            </table>
-        </div>
+                    </ul>
+                </td>
+            </tr>
+        </tbody>
+    </table>
+</div>
+
     )
 }
 
